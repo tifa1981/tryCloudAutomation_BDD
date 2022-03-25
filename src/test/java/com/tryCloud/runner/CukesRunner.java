94973ae28a6c691b1203d1db802e28941b720003
YanchunZhou <tifa1981@gmail.com>
2022-03-25 14:33:14 -0400

    tryCloud project created

---
+++ src/test/java/com/tryCloud/runner/CukesRunner.java
@@ -0,0 +1,24 @@
+package com.tryCloud.runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {
+                "html:target/cucumber-report.html",
+                "rerun:target/rerun.txt",
+                "me.jvt.cucumber.report.PrettyReports:target/cucumber"
+        },
+        features = "src/test/resources/features",
+        glue = "net/seamlessly/step_definitions",
+        dryRun = false,
+        tags = "",
+        publish = true
+)
+
+
+public class CukesRunner {
+}
