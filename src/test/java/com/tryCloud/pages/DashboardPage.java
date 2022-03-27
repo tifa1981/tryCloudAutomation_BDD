58d260a826daa4b3be78ce423d5836eb6e56f71d
YanchunZhou <tifa1981@gmail.com>
2022-03-27 12:45:42 -0400

    dashboard page created with locators

---
+++ src/test/java/com/tryCloud/pages/DashboardPage.java
@@ -0,0 +1,22 @@
+package com.tryCloud.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DashboardPage extends BasePage{
+
+    @FindBy(xpath = "//span[text()='Dashboard']")
+    public WebElement dashboard;
+
+    @FindBy(xpath="//span[.='Files']")
+    public WebElement files ;
+
+    @FindBy(xpath ="//span[.='Talk']")
+    public WebElement talk;
+
+    @FindBy(xpath="(//span[.='Contacts'])[1]")
+    public WebElement contacts ;
+
+    @FindBy(xpath ="//div[@class='header-menu unified-search header-menu--opened']")
+    public WebElement serachIcon;
+}
