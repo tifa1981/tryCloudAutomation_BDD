6beda61dd9d2f51004329fb8f372253138bae536
YanchunZhou <tifa1981@gmail.com>
2022-03-27 17:44:07 -0400

    dashboard page created with locators

---
+++ src/test/java/com/tryCloud/pages/FilesPage.java
@@ -0,0 +1,18 @@
+package com.tryCloud.pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class FilesPage extends BasePage{
+
+    @FindBy(css = "button[class='settings-button']")
+    public WebElement settingsBtn;
+
+    @FindBy(css= "div[id='files-app-extra-settings']")
+    public List<WebElement> checkBoxes;
+
+
+
+}
---
+++ src/test/resources/features/US10_UpdateSettings.feature
@@ -0,0 +1,12 @@
+Feature: As a user, I should be able to update settings.
+
+
+
+  @wip
+  Scenario: Verify users are able to update settings
+    Given user on the dashboard page
+    When the user clicks the Files module
+    And user clicks Settings on the left bottom corner
+    Then the user should be able to click any buttons
+
+
