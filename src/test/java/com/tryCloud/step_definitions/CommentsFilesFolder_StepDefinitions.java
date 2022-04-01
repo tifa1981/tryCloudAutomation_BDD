f6c7f0c3d7720f0e813858f1d59fced174e7575d
Tariq Ejaz <mtariq_ejaz@yahoo,com>
2022-04-01 18:10:59 -0400

    US9 Done.

--- src/test/java/com/tryCloud/step_definitions/CommentsFilesFolder_StepDefinitions.java
+++ src/test/java/com/tryCloud/step_definitions/CommentsFilesFolder_StepDefinitions.java
@@ -8,49 +8,60 @@ import com.tryCloud.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import java.util.Locale;
 
 public class CommentsFilesFolder_StepDefinitions {
+
     // The users log in with valid credentials
     LoginPage loginPage = new LoginPage();
     DashboardPage dashboardPage = new DashboardPage();
     FilesPage filesPage = new FilesPage();
     String textIs = "Im done for this project, what is next";
+
     public void the_users_log_in_with_valid_credentials() {
         Driver.getDriver().get("http://qa2.trycloud.net");
         loginPage.loginWithConfig();
     }
 
     @When("the user clicks the {string} module")
-    public void the_user_clicks_the_module(String file) {
-        dashboardPage.filesModule.click();
+    public void the_user_clicks_the_module(String string) {
+
+        BrowserUtils.DashboardPage(string);
     }
 
     @When("user click action-icon from any file on the page")
     public void user_click_action_icon_from_any_file_on_the_page() {
+        BrowserUtils.sleep(2);
         filesPage.actionIcon.click();
+
     }
 
     @When("user choose the {string} option")
-    public void user_choose_the_option(String Details) {
+    public void user_choose_the_option(String string) {
         BrowserUtils.sleep(2);
-        filesPage.details.click();
+        BrowserUtils.FilePage(string);
     }
 
     @When("user write a comment inside the input box")
     public void user_write_a_comment_inside_the_input_box() {
-       filesPage.commentsBox.click();
+        filesPage.commentsBox.click();
     }
+
     @When("user click the submit button to post it")
     public void user_click_the_submit_button_to_post_it() {
-        filesPage.textBox.sendKeys(textIs+ Keys.ENTER);
+        filesPage.textBox.sendKeys(textIs + Keys.ENTER);
     }
 
     @Then("Verify the comment is displayed in the comment section.")
     public void verify_the_comment_is_displayed_in_the_comment_section() {
-        System.out.println("filesPage.verifyText.getText() = " + filesPage.verifyText.getText());
+        //
         Assert.assertTrue(textIs.equals(filesPage.verifyText.getText()));
 
     }
-
 }
+
+
