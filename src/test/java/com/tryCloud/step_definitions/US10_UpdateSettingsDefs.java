d44c30f67da00ca1593953d66ec7ef5ac808a211
YanchunZhou <tifa1981@gmail.com>
2022-04-02 19:08:18 -0400

    US10 is done, final version

--- src/test/java/com/tryCloud/step_definitions/US10_UpdateSettingsDefs.java
+++ src/test/java/com/tryCloud/step_definitions/US10_UpdateSettingsDefs.java
@@ -8,19 +8,18 @@ import com.tryCloud.utilities.Driver;
 import io.cucumber.java.en.*;
 import org.junit.Assert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 
 import java.awt.*;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
-import java.util.List;
 
-public class UpdateSettingsDefs {
+public class US10_UpdateSettingsDefs {
 
     LoginPage loginPage = new LoginPage();
     DashboardPage dashboard = new DashboardPage();
     FilesPage files = new FilesPage();
+    String storageBeforeUpload;
 
     @Given("user on the dashboard page")
     public void user_on_the_dashboard_page() {
@@ -29,10 +28,12 @@ public class UpdateSettingsDefs {
         loginPage.loginWithConfig();
 
     }
-    @When("user clicks the Files module")
-    public void user_clicks_the_files_module() {
-        BrowserUtils.waitForPresenceOfElement(By.xpath("//li[@data-id='files']"), 5);
-        dashboard.filesModule.click();
+
+    @When("user clicks the {string} module")
+    public void user_Clicks_The_Module(String files) {
+        dashboard.clickModule(files);
+
+
     }
 
     @When("user clicks Settings on the left bottom corner")
@@ -40,60 +41,71 @@ public class UpdateSettingsDefs {
         //BrowserUtils.waitFor(5);
         files.settingsBtn.click();
     }
+
     @Then("user should be able to click any buttons")
     public void user_should_be_able_to_click_any_buttons() {
-        //  WebElement checkBox =Driver.getDriver().findElement(By.xpath("(//div[@id='app-settings-content']//label)"));
         for (int i = 1; i <= 3; i++) {
             WebElement checkBox = Driver.getDriver().findElement(By.xpath("(//div[@id='app-settings-content']//label)[" + i + "]"));
-            BrowserUtils.waitFor(5);
-            checkBox.click();
-            Assert.assertTrue(checkBox.isDisplayed());
+            BrowserUtils.waitFor(3);
+            if (!checkBox.isSelected()) {
+                checkBox.click();
+            }if (checkBox.isSelected()) {
+                checkBox.click();
+            }
+          Assert.assertNull(checkBox.getAttribute("checked"));
+           // checkBox.click();
+           // Assert.assertNotNull(checkBox.getAttribute("checked"));
         }
 
+
+
     }
+
+
+
 
     @And("user checks the current storage usage")
     public void userChecksTheCurrentStorageUsage() {
         System.out.println("Storage usage before upload = " + files.numberOfUsage.getText());
-
+        storageBeforeUpload = files.numberOfUsage.getText();
     }
 
     @And("user uploads file with the upload file option")
     public void userUploadsFileWithTheUploadFileOption() {
-            // BrowserUtils.waitFor(2);
-            files.icon_add.click();
+        // BrowserUtils.waitFor(2);
+        files.icon_add.click();
 
-            String path= "D:\\soft skills\\1.Interview Prep - SDLC & Agile & STLC - Google Docs.pdf";
-            BrowserUtils.waitFor(3);
+        String path = "D:\\soft skills\\1.Interview Prep - SDLC & Agile & STLC - Google Docs.pdf";
+        BrowserUtils.waitFor(3);
 
-            files.uploadFileBtn.click();// Click on browse option on the webpage
-            BrowserUtils.waitFor(2);// suspending execution for specified time period
+        files.uploadFileBtn.click();// Click on browse option on the webpage
+        BrowserUtils.waitFor(2);// suspending execution for specified time period
 
-            // creating object of Robot class
-            Robot rb = null;
-            try {
-                rb = new Robot();
-            } catch (AWTException e) {
-                e.printStackTrace();
-            }
-
-            // copying File path to Clipboard
-            StringSelection str = new StringSelection(path);
-            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
-
-            // press Contol+V for pasting
-            rb.keyPress(KeyEvent.VK_CONTROL);
-            rb.keyPress(KeyEvent.VK_V);
-
-            // release Contol+V for pasting
-            rb.keyRelease(KeyEvent.VK_CONTROL);
-            rb.keyRelease(KeyEvent.VK_V);
-
-            // for pressing and releasing Enter
-            rb.keyPress(KeyEvent.VK_ENTER);
-            rb.keyRelease(KeyEvent.VK_ENTER);
-            BrowserUtils.waitFor(3);
+        // creating object of Robot class
+        Robot rb = null;
+        try {
+            rb = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
         }
+
+        // copying File path to Clipboard
+        StringSelection str = new StringSelection(path);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(str, null);
+
+        // press Contol+V for pasting
+        rb.keyPress(KeyEvent.VK_CONTROL);
+        rb.keyPress(KeyEvent.VK_V);
+
+        // release Contol+V for pasting
+        rb.keyRelease(KeyEvent.VK_CONTROL);
+        rb.keyRelease(KeyEvent.VK_V);
+
+        // for pressing and releasing Enter
+        rb.keyPress(KeyEvent.VK_ENTER);
+        rb.keyRelease(KeyEvent.VK_ENTER);
+        BrowserUtils.waitFor(3);
+    }
 
 
     @And("user refreshes the page")
@@ -107,8 +119,9 @@ public class UpdateSettingsDefs {
     public void userShouldBeAbleToSeeStorageUsageIsIncreased() {
         WebElement numberOfUsage2 = files.numberOfUsage;
         System.out.println("Storage usage after upload = " + numberOfUsage2.getText());
-        double n1 = Double.parseDouble(files.numberOfUsage.getText().substring(0,3));
-        double n2 = Double.parseDouble(numberOfUsage2.getText().substring(0,4));
+        double n1 = Double.parseDouble(storageBeforeUpload.substring(0, storageBeforeUpload.indexOf(" ")));
+        String storageAfterUpload = numberOfUsage2.getText();
+        double n2 = Double.parseDouble(storageAfterUpload.substring(0, storageAfterUpload.indexOf(" ")));
         System.out.println("n1 = " + n1);
         System.out.println("n2 = " + n2);
 
@@ -116,4 +129,5 @@ public class UpdateSettingsDefs {
         Assert.assertTrue(b);
 
     }
-    }
+
+}
